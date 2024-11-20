e398a7ba6f5c3e18a75d2c283bbcb5d9997b6da6
Joel Chelliah <joel.chelliah@entur.org>
2024-11-20 21:43:51 +0100

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // React Rules
+    'react/prop-types': 'off', // Since we're using TypeScript
+    'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+
+    // TypeScript Rules
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-var-requires': 'error',
+
+    // Import Rules
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling']],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+
+    // General Rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-unused-expressions': 'error',
+    'no-unused-vars': 'off', // Using TypeScript's no-unused-vars instead
+  },
+}
