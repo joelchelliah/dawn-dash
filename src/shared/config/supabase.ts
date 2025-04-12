e9a0dca60bb92b4d664f4f4c512b41731e4d7430
Joel Chelliah <joel.chelliah@entur.org>
2025-04-12 14:24:15 +0200

    Updated supabase config

--- src/shared/config/supabase.ts
+++ src/shared/config/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// REACT_APP_ prefix is for local development
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_KEY || process.env.SUPABASE_KEY
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables')
