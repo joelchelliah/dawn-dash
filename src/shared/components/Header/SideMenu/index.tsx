e89bc2f9febb54d48f596400f90d07501b207b53
Joel Chelliah <joel.chelliah@entur.org>
2025-04-14 19:22:03 +0200

    Moved side-menu into separate component

---
+++ src/shared/components/Header/SideMenu/index.tsx
@@ -0,0 +1,138 @@
+import { useState, useEffect, useRef } from 'react'
+
+import { Link } from 'react-router-dom'
+import cx from 'classnames'
+
+import { HamburgerIcon } from '../../../utils/icons'
+import { AbracadabraImageUrl, DashImageUrl, RushedForgeryImageUrl } from '../../../utils/imageUrls'
+import GradientLink from '../../GradientLink'
+import InfoModal from '../../Modals/InfoModal'
+
+import styles from './index.module.scss'
+
+interface SideMenuProps {
+  currentPage: 'speedruns' | 'cardex'
+}
+
+const SideMenu = ({ currentPage }: SideMenuProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isAboutInfoOpen, setIsAboutInfoOpen] = useState(false)
+  const menuRef = useRef<HTMLDivElement>(null)
+  const hamburgerRef = useRef<HTMLButtonElement>(null)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isMenuOpen &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        hamburgerRef.current &&
+        !hamburgerRef.current.contains(event.target as Node)
+      ) {
+        setIsMenuOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [isMenuOpen])
+
+  const speedrunsLinkContainerClassNames = cx(
+    styles['side-menu__nav-link-container'],
+    currentPage === 'speedruns' ? styles['side-menu__nav-link-container--active'] : ''
+  )
+  const cardexLinkContainerClassNames = cx(
+    styles['side-menu__nav-link-container'],
+    currentPage === 'cardex' ? styles['side-menu__nav-link-container--active'] : ''
+  )
+
+  return (
+    <>
+      <div className={styles['hamburger']}>
+        <button
+          ref={hamburgerRef}
+          className={styles['hamburger__button']}
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <HamburgerIcon className={styles['hamburger__button__icon']} />
+        </button>
+      </div>
+
+      <div
+        ref={menuRef}
+        className={`${styles['side-menu']} ${isMenuOpen ? styles['side-menu--open'] : ''}`}
+      >
+        <nav className={styles['side-menu__nav']}>
+          <div className={speedrunsLinkContainerClassNames}>
+            <Link to="/" className={styles['side-menu__nav-link']}>
+              <img
+                src={DashImageUrl}
+                alt="Speedruns logo"
+                className={styles['side-menu__nav-link__icon']}
+              />
+              Speedruns
+            </Link>
+          </div>
+
+          <div className={cardexLinkContainerClassNames}>
+            <Link to="/codex/cards" className={styles['side-menu__nav-link']}>
+              <img
+                src={AbracadabraImageUrl}
+                alt="Cardex logo"
+                className={styles['side-menu__nav-link__icon']}
+              />
+              Cardex
+            </Link>
+          </div>
+
+          <div className={styles['side-menu__nav-link-container']}>
+            <Link
+              to=""
+              className={styles['side-menu__nav-link']}
+              onClick={() => setIsAboutInfoOpen(true)}
+            >
+              <img
+                src={RushedForgeryImageUrl}
+                alt="About logo"
+                className={styles['side-menu__nav-link__icon']}
+              />
+              About
+            </Link>
+          </div>
+        </nav>
+      </div>
+
+      <InfoModal isOpen={isAboutInfoOpen} onClose={() => setIsAboutInfoOpen(false)}>
+        <h3>Dawn-Dash</h3>
+
+        <p>
+          <b>Dawncaster</b> speedrun charts for all game modes and difficulties, based on
+          player-submitted data from{' '}
+          <GradientLink text="blightbane.io" url="https://blightbane.io/" />.
+        </p>
+        <p>
+          The player names linked to these runs are their Discord accounts from the official{' '}
+          <GradientLink text="Dawncaster Discord" url="https://discord.gg/pfeMG9c" />.
+        </p>
+        <div className={styles['info-divider']} />
+
+        <h3>Cardex</h3>
+
+        <p className={styles['info-last-paragraph']}>
+          A codex and multi-search tool for all the cards available in <b>Dawncaster</b>. Has
+          several options for filtering, tracking, and formatting the output, to help you plan out
+          your run!
+        </p>
+      </InfoModal>
+    </>
+  )
+}
+
+export default SideMenu
--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,13 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
-
-import { Link } from 'react-router-dom'
-import cx from 'classnames'
-
-import { HamburgerIcon } from '../../utils/icons'
-import { AbracadabraImageUrl, DashImageUrl, RushedForgeryImageUrl } from '../../utils/imageUrls'
-import GradientLink from '../GradientLink'
-import InfoModal from '../Modals/InfoModal'
-
+import SideMenu from './SideMenu'
 import styles from './index.module.scss'
 
 interface HeaderProps {
@@ -19,42 +10,6 @@ interface HeaderProps {
 }
 
 const Header = ({ onLogoClick, logoSrc, title, subtitle, currentPage }: HeaderProps) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const [isAboutInfoOpen, setIsAboutInfoOpen] = useState(false)
-  const menuRef = useRef<HTMLDivElement>(null)
-  const hamburgerRef = useRef<HTMLButtonElement>(null)
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        isMenuOpen &&
-        menuRef.current &&
-        !menuRef.current.contains(event.target as Node) &&
-        hamburgerRef.current &&
-        !hamburgerRef.current.contains(event.target as Node)
-      ) {
-        setIsMenuOpen(false)
-      }
-    }
-
-    document.addEventListener('mousedown', handleClickOutside)
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [isMenuOpen])
-
-  const speedrunsLinkContainerClassNames = cx(
-    styles['side-menu__nav-link-container'],
-    currentPage === 'speedruns' ? styles['side-menu__nav-link-container--active'] : ''
-  )
-  const cardexLinkContainerClassNames = cx(
-    styles['side-menu__nav-link-container'],
-    currentPage === 'cardex' ? styles['side-menu__nav-link-container--active'] : ''
-  )
   return (
     <div className={styles['header']}>
       <div className={styles['logo-and-title']} onClick={onLogoClick}>
@@ -65,80 +20,7 @@ const Header = ({ onLogoClick, logoSrc, title, subtitle, currentPage }: HeaderPr
         </div>
       </div>
 
-      <div className={styles['hamburger']}>
-        <button
-          ref={hamburgerRef}
-          className={styles['hamburger__button']}
-          onClick={toggleMenu}
-          aria-label="Toggle menu"
-        >
-          <HamburgerIcon className={styles['hamburger__button__icon']} />
-        </button>
-      </div>
-
-      <div
-        ref={menuRef}
-        className={`${styles['side-menu']} ${isMenuOpen ? styles['side-menu--open'] : ''}`}
-      >
-        <nav className={styles['side-menu__nav']}>
-          <div className={speedrunsLinkContainerClassNames}>
-            <Link to="/" className={styles['side-menu__nav-link']}>
-              <img
-                src={DashImageUrl}
-                alt="Speedruns logo"
-                className={styles['side-menu__nav-link__icon']}
-              />
-              Speedruns
-            </Link>
-          </div>
-          <div className={cardexLinkContainerClassNames}>
-            <Link to="/codex/cards" className={styles['side-menu__nav-link']}>
-              <img
-                src={AbracadabraImageUrl}
-                alt="Cardex logo"
-                className={styles['side-menu__nav-link__icon']}
-              />
-              Cardex
-            </Link>
-          </div>
-          <div className={styles['side-menu__nav-link-container']}>
-            <Link
-              to=""
-              className={styles['side-menu__nav-link']}
-              onClick={() => setIsAboutInfoOpen(true)}
-            >
-              <img
-                src={RushedForgeryImageUrl}
-                alt="About logo"
-                className={styles['side-menu__nav-link__icon']}
-              />
-              About
-            </Link>
-          </div>
-        </nav>
-      </div>
-      <InfoModal isOpen={isAboutInfoOpen} onClose={() => setIsAboutInfoOpen(false)}>
-        <h3>Dawn-Dash</h3>
-
-        <p>
-          <b>Dawncaster</b> speedrun charts for all game modes and difficulties, based on
-          player-submitted data from{' '}
-          <GradientLink text="blightbane.io" url="https://blightbane.io/" />.
-        </p>
-        <p>
-          The player names linked to these runs are their Discord accounts from the official{' '}
-          <GradientLink text="Dawncaster Discord" url="https://discord.gg/pfeMG9c" />.
-        </p>
-        <div className={styles['info-divider']} />
-
-        <h3>Cardex</h3>
-
-        <p className={styles['info-last-paragraph']}>
-          A codex and multi-search tool for all the cards available in <b>Dawncaster</b>. Has
-          several options for filtering, tracking, and formatting the output, to help you plan out
-          your run!
-        </p>
-      </InfoModal>
+      <SideMenu currentPage={currentPage} />
     </div>
   )
 }
