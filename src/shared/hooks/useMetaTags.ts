59176a605cda66b66e6a4f605c9dd96621312224
Joel Chelliah <joel.chelliah@entur.org>
2025-07-13 10:49:09 +0200

    Removed default metatags from public/index

--- src/shared/hooks/useMetaTags.ts
+++ src/shared/hooks/useMetaTags.ts
@@ -7,6 +7,7 @@ interface MetaTags {
   ogDescription: string
   ogImage: string
   ogUrl: string
+  ogSiteName: string
   twitterImage: string
 }
 
@@ -18,6 +19,7 @@ const META_TAGS_BY_ROUTE: Record<string, MetaTags> = {
     ogDescription: 'Dawncaster speedrun charts, records and more!',
     ogImage: 'https://dawn-dash.com/og-image-dawndash.png',
     ogUrl: 'https://dawn-dash.com',
+    ogSiteName: 'dawn-dash.com',
     twitterImage: '/og-image-dawndash.png',
   },
   '/codex/cards': {
@@ -29,6 +31,7 @@ const META_TAGS_BY_ROUTE: Record<string, MetaTags> = {
       'Dawncaster card search & filter - Browse and search through all Dawncaster cards with advanced filtering options',
     ogImage: 'https://dawn-dash.com/og-image-cardex.png',
     ogUrl: 'https://dawn-dash.com/codex/cards',
+    ogSiteName: 'dawn-dash.com/codex/cards',
     twitterImage: '/og-image-cardex.png',
   },
   '/codex/wip/super-secret/dont-look/talents': {
@@ -39,9 +42,10 @@ const META_TAGS_BY_ROUTE: Record<string, MetaTags> = {
     ogDescription:
       'Dawncaster talent search & filter - Browse and search through all Dawncaster talents with advanced filtering options',
     // TODO: Create custom OG image for talents page (e.g., og-image-talents.png)
-    ogImage: 'https://dawn-dash.com/og-image-dawndash.png',
+    ogImage: 'https://dawn-dash.com/og-image-default.png',
     ogUrl: 'https://dawn-dash.com/codex/wip/super-secret/dont-look/talents',
-    twitterImage: '/og-image-dawndash.png',
+    ogSiteName: 'dawn-dash.com',
+    twitterImage: '/og-image-default.png',
   },
 }
 
