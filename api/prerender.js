abc3895692c4bc1fca44c1668d91e1c600f44bf3
Joel Chelliah <joel.chelliah@entur.org>
2025-07-13 09:53:24 +0200

    Prerendering setup using an edge function

---
+++ api/prerender.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { path } = req.query
+  const prerenderUrl = `https://service.prerender.io/https://dawn-dash.com${path || '/'}`
+  const response = await fetch(prerenderUrl, {
+    headers: {
+      'X-Prerender-Token': process.env.PRERENDER_TOKEN,
+      'User-Agent': req.headers['user-agent'],
+    },
+  })
+  const html = await response.text()
+  res.setHeader('Content-Type', 'text/html')
+  res.status(response.status).send(html)
+}
