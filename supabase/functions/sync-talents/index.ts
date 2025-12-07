1f2cc97bc20d2fc85151cde6882f0fa27f2b65a8
Joel Chelliah <joel.chelliah@entur.org>
2025-12-07 20:28:04 +0100

    Finally fixed keep supabase alive.. maybe?

--- supabase/functions/sync-talents/index.ts
+++ supabase/functions/sync-talents/index.ts
@@ -94,13 +94,7 @@ serve(async (req) => {
 
     const supabaseClient = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
-      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
-      {
-        global: {
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          headers: { Authorization: req.headers.get('Authorization')! },
-        },
-      }
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
     )
 
     console.info('Fetching talents from Blightbane API...')
