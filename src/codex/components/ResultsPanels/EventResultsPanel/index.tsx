119496fce53ebbfa28763094722d3a2884826d0c
Joel Chelliah <joel.chelliah@entur.org>
2025-12-30 21:09:01 +0100

    Not-mapped-out message

--- src/codex/components/ResultsPanels/EventResultsPanel/index.tsx
+++ src/codex/components/ResultsPanels/EventResultsPanel/index.tsx
@@ -1,4 +1,5 @@
 import { createCx } from '@/shared/utils/classnames'
+import GradientLink from '@/shared/components/GradientLink'
 
 import { Event } from '@/codex/types/events'
 
@@ -14,23 +15,39 @@ interface EventResultsPanelProps {
 }
 
 const EventResultsPanel = ({ selectedEvent }: EventResultsPanelProps) => {
+  const eventName = selectedEvent?.name ?? 'UNNAMED-EVENT'
+  const blightbaneLink = `https://www.blightbane.io/event/${eventName.replaceAll(' ', '_')}`
   return (
     <div className={cx('results-panel')}>
       <PanelHeader type="Results" />
 
       <div className={cx('results-container')}>
-        {selectedEvent && !selectedEvent.excluded && <EventTree event={selectedEvent} />}
+        {selectedEvent && !selectedEvent.excluded && (
+          <>
+            <EventTree event={selectedEvent} />
+
+            <div className={cx('info-message')}>
+              Visit <strong>Blightbane</strong> to walk through this event step-by-step:{` `}
+              <GradientLink text={eventName} url={blightbaneLink} />
+            </div>
+          </>
+        )}
 
         {selectedEvent && selectedEvent.excluded && (
-          <div className={cx('excluded-message')}>
-            This event has been excluded from visualization.
+          <div className={cx('info-message')}>
+            This event hasn&apos;t been fully mapped out. It&apos;s most likely a pure story event
+            with no special rewards, or choices that effect your run.
+            <br />
+            <br />
+            You can still explore the event on <strong>Blightbane</strong>:
+            <br />
+            <br />
+            <GradientLink text={eventName} url={blightbaneLink} />
           </div>
         )}
 
         {!selectedEvent && (
-          <div className={cx('no-event-message')}>
-            No event selected! Select an event to visualize.
-          </div>
+          <div className={cx('info-message')}>No event selected! Select an event to visualize.</div>
         )}
       </div>
     </div>
