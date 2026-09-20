5a484cf6775388b3e9f7683cb00695273874e866
Joel Chelliah <joel.chelliah@bekk.no>
2026-09-20 12:28:54 +0200

    Fixed artworkLink mobile issues

--- src/codex/components/BootyPanels/shared/PoolSourceList/index.tsx
+++ src/codex/components/BootyPanels/shared/PoolSourceList/index.tsx
@@ -6,7 +6,7 @@ import { ArtworkLinkLayout, ArtworkLinkList } from '../ArtworkLinkList'
 import { CardSourceItem, EventSourceItem } from '../ArtworkSourceItem'
 
 const ARTWORK_SIZE = 40
-const ARTWORK_SIZE_MOBILE = 28
+const ARTWORK_SIZE_MOBILE = 24
 
 interface PoolSourceListProps {
   sources: PoolReachedBy[]
