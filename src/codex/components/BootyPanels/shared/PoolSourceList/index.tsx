018ed7b9f1c1baf1a269e9de68647368c8931558
Joel Chelliah <joel.chelliah@bekk.no>
2026-09-21 14:22:30 +0200

    Allow larger mobile artwork where possible

--- src/codex/components/BootyPanels/shared/PoolSourceList/index.tsx
+++ src/codex/components/BootyPanels/shared/PoolSourceList/index.tsx
@@ -7,21 +7,32 @@ import { CardSourceItem, EventSourceItem } from '../ArtworkSourceItem'
 
 const ARTWORK_SIZE = 40
 const ARTWORK_SIZE_MOBILE = 24
+const ARTWORK_SIZE_MOBILE_LARGE = 32
 
 interface PoolSourceListProps {
   sources: PoolReachedBy[]
   layout: ArtworkLinkLayout
   unshaded?: boolean
+  allowLargerMobileArtwork?: boolean
 }
 
-function PoolSourceList({ sources, layout, unshaded }: PoolSourceListProps): JSX.Element | null {
+function PoolSourceList({
+  sources,
+  layout,
+  unshaded,
+  allowLargerMobileArtwork = false,
+}: PoolSourceListProps): JSX.Element | null {
   if (sources.length === 0) return null
+
+  const artworkSizeMobile = allowLargerMobileArtwork
+    ? ARTWORK_SIZE_MOBILE_LARGE
+    : ARTWORK_SIZE_MOBILE
 
   return (
     <ArtworkLinkList layout={layout} unshaded={unshaded}>
       {sources.map(({ name, sourceType }) => {
         const key = `${name}-${sourceType}`
-        const sizes = { artworkSize: ARTWORK_SIZE, artworkSizeMobile: ARTWORK_SIZE_MOBILE }
+        const sizes = { artworkSize: ARTWORK_SIZE, artworkSizeMobile }
 
         return sourceType === 'event' ? (
           <EventSourceItem key={key} name={name} {...sizes} />
